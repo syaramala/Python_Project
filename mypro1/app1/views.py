2cd32a3afd8e3758bfb707f9939b2b5b95bba2e8
sridhar <ysrnjc2017@gmail.com>
2024-12-14 18:31:08 +0530

    vv

---
+++ mypro1/app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+     return render(request, 'home.html')
+
+def register(request):
+     return render(request, 'register.html')
+
+def login(request):
+     return render(request, 'login.html')
+
+def logout(request):
+     return render(request, 'logout.html')
